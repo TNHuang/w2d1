f6e586973f0275066d562a55c1408a453814858d
kenny huang <khuangtai@gmail.com>
2014-09-15 11:53:00 -0400

    Begin queue of tiles to check after selection

--- minesweeper.rb
+++ minesweeper.rb
@@ -4,13 +4,39 @@ class Board
 
   attr_accessor :tiles
 
-  def self.create_tiles
-    Array.new(9) {Array.new(9) { Tile.new }}
+  def initialize(bomb_count = 20)
+    @tiles, @bomb_count = create_tiles, bomb_count
+    seed_bombs
   end
 
-  def initialize(bomb_count = 20)
-    @tiles, @bomb_count = Board.create_tiles, bomb_count
-    seed_bombs
+  def select_tile(pos)
+    return if tile[pos].bomb
+
+    tiles_to_check = [tiles[pos]]
+    until tiles_to_check.empty?
+      tile = tiles_to_check.shift
+      tiles[pos].revealed = true
+
+      # if tile.next_to_bomb?
+      #   get neighbors who are not next to bombs
+      # else
+      #   add all neighbors to tiles_to_check
+    end
+  end
+
+  def [](pos)
+    i, j = pos[0], pos[1]
+    tiles[i][j]
+  end
+
+  def []=(pos, value)
+    i, j = pos[0], pos[1]
+    tiles[i][j] = value
+  end
+
+  protected
+  def create_tiles
+    Array.new(9) {Array.new(9) { Tile.new(self) }}
   end
 
   def seed_bombs
@@ -27,10 +53,10 @@ class Board
 end
 
 class Tile
-  attr_accessor :bomb
+  attr_accessor :bomb, :revealed
 
-  def initialize(bomb = false)
-    @bomb = bomb
+  def initialize(board, bomb = false)
+    @board, @bomb, @revealed = board, bomb, false
   end
 
 end
